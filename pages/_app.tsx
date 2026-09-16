55e61ab106b23f924241b70383ea61c1173ab4f5
Egor Denisov <designer.egor@ya.ru>
2026-09-15 20:00:43 -0400

    fix: use static examples layout in app shell

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import type { LayoutProps } from '@vercel/examples-ui/layout'
-import { getLayout } from '@vercel/examples-ui'
+import { Layout } from '@vercel/examples-ui'
 import '@vercel/examples-ui/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component)
   const title = pageProps.dictionary?.title || 'IP location'
   const description = pageProps.dictionary?.subtitle
 
